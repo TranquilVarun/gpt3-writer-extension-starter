8fd31c5054dc3038e533ae03a4c45071616995cf
TranquilVarun <108307732+TranquilVarun@users.noreply.github.com>
2023-01-19 21:51:32 -0600

    Commit 2

--- index.js
+++ index.js
@@ -11,19 +11,6 @@ document
   .getElementById('change_key_button')
   .addEventListener('click', changeKey);
 
-const saveKey = () => {}
-
-const changeKey = () => {}
-
-document.getElementById('save_key_button').addEventListener('click', saveKey);
-document
-  .getElementById('change_key_button')
-  .addEventListener('click', changeKey);
-
-  const encode = (input) => {
-    return btoa(input);
-  };
-
 const saveKey = () => {
    const input = document.getElementById('key_input');
   
