38a883a4773fed872a5eb41df7961ca04d3ea46a
TranquilVarun <108307732+TranquilVarun@users.noreply.github.com>
2023-01-19 22:06:20 -0600

    Fix2

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const encode = (input) => {
+  return btoa(input);
+};
+
 const checkForKey = () => {
   return new Promise((resolve, reject) => {
     chrome.storage.local.get(['openai-key'], (result) => {
@@ -6,45 +10,36 @@ const checkForKey = () => {
   });
 };
 
-document.getElementById('save_key_button').addEventListener('click', saveKey);
-document
-  .getElementById('change_key_button')
-  .addEventListener('click', changeKey);
+const saveKey = () => {
+  const input = document.getElementById('key_input');
 
-  const encode = (input) => {
-    return btoa(input);
-  };
+  if (input) {
+    const { value } = input;
 
-  const saveKey = () => {
-    const input = document.getElementById('key_input');
-  
-    if (input) {
-      const { value } = input;
-  
-      // Encode String
-      const encodedValue = encode(value);
-  
-      // Save to google storage
-      chrome.storage.local.set({ 'openai-key': encodedValue }, () => {
-        document.getElementById('key_needed').style.display = 'none';
-        document.getElementById('key_entered').style.display = 'block';
-      });
-    }
-  };
+    // Encode String
+    const encodedValue = encode(value);
 
-  const changeKey = () => {
-    document.getElementById('key_needed').style.display = 'block';
-    document.getElementById('key_entered').style.display = 'none';
-  };
-
-document.getElementById('save_key_button').addEventListener('click', saveKey);
-document
-  .getElementById('change_key_button')
-  .addEventListener('click', changeKey);
-
-  checkForKey().then((response) => {
-    if (response) {
+    // Save to google storage
+    chrome.storage.local.set({ 'openai-key': encodedValue }, () => {
       document.getElementById('key_needed').style.display = 'none';
       document.getElementById('key_entered').style.display = 'block';
-    }
-  });
+    });
+  }
+};
+
+const changeKey = () => {
+  document.getElementById('key_needed').style.display = 'block';
+  document.getElementById('key_entered').style.display = 'none';
+};
+
+document.getElementById('save_key_button').addEventListener('click', saveKey);
+document
+  .getElementById('change_key_button')
+  .addEventListener('click', changeKey);
+
+checkForKey().then((response) => {
+  if (response) {
+    document.getElementById('key_needed').style.display = 'none';
+    document.getElementById('key_entered').style.display = 'block';
+  }
+});
